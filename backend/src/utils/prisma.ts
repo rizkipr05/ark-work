ec5659995daa3528ff1a8287795d8da196236fd8
aditiaputra1212 <aditiaputra88890@gmail.com>
2025-09-23 10:08:26 +0700

    sistem apply

--- backend/src/utils/prisma.ts
+++ backend/src/utils/prisma.ts
@@ -1,7 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+// backend/src/utils/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  log: ["error", "warn"],
-});
+const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['warn', 'error'],
+  });
+
+// agar 1 instance di dev (hot reload)
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
